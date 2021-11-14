9cebf3797744bc3a31e8c8d512c1ec4ad406e855
Nicoli430 <nicoli.dev.430@gmail.com>
2021-11-14 12:50:28 -0600

    fix movie selection overflow movie card

--- lib/projects/travel_app/travel_app.dart
+++ lib/projects/travel_app/travel_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_projects/projects/travel_app/theme/theme.dart';
-import 'package:flutter_projects/projects/travel_app/ui/feed/feed_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TravelApp extends StatelessWidget {
   const TravelApp({Key? key}) : super(key: key);
@@ -9,8 +8,14 @@ class TravelApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: TravelTheme.theme,
-      home: const FeedScreen(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(),
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('New York'),
+        ),
+      ),
     );
   }
 }
